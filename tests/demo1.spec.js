ace28fb68b090b22d119ee32256c7d84d119ed5a
bhushan <bhushan011294@gmail.com>
2025-11-11 13:44:24 +0530

    Firts Playwright Project

---
+++ tests/demo1.spec.js
@@ -0,0 +1,21 @@
+import {test , expect} from '@playwright/test'
+
+test ('Locators' , async ({page}) =>
+    {
+ await page.goto('https://demoblaze.com/');
+  await page.getByRole('link', {name :'Sign up'}).click();
+
+  await page.getByLabel('Username').fill('Chandler');
+  await page.getByLabel('Password').fill('Chandler@123');
+
+  //await page.getByRole ('link' , {name : 'Close'}).click();
+ // await expect(page.getByText ('Samsung')).toBeVisible();
+
+  //await page.getByRole ('button', {name : 'Sign up'}).click();
+
+// await page.getByRole('link' , {name : 'About us'}).click();
+ // await page.getByTitle ('play video').click();
+
+
+ // await page.getByLabel
+});
---
+++ tests/test-1.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  // Recording...
+  await page.goto('https://playwright.dev/');
+await page.getByRole('link', { name: 'Get started' }).click();
+await page.getByRole('tab', { name: 'yarn' }).nth(1).click();
+await page.getByRole('link', { name: 'Setting up CI' }).click();
+});
